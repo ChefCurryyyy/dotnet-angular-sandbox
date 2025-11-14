8a12bc81e95d4caed3ae41e71ebd8cf7632b9a09
Ahmad <ahmad08ali@gmail.com>
2025-11-14 14:41:23 +0100

    add endpoints for todo class (test endpoints)

---
+++ backend/Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DotnetAngularSandbox.Backend.Data;
+using DotnetAngularSandbox.Backend.Models;
+
+namespace DotnetAngularSandbox.Backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodoController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public TodoController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetTodos()
+    {
+        return Ok(await _context.TodoItems.ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTodo([FromBody] TodoItem todo)
+    {
+        _context.TodoItems.Add(todo);
+        await _context.SaveChangesAsync();
+        return Ok(todo);
+    }
+}
---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DotnetAngularSandbox.Backend.Models;
+
+namespace DotnetAngularSandbox.Backend.Data;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    public DbSet<TodoItem> TodoItems { get; set; }
+}
